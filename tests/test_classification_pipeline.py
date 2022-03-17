e7a7525522948ed510cb57cde61e33de5fe175ca
Giorgio Savastano <giorgio.savastano@uniroma1.it>
2022-03-17 09:19:40 +0100

    modified test

--- tests/test_classification_pipeline.py
+++ tests/test_classification_pipeline.py
@@ -18,8 +18,8 @@ class TestDistMatrix(TestCase):
 
     @pytest.mark.unit
     def test_distance_matrix(self):
-        matrix_arrays = np.random.random((1000, 10, 50))
-        dist_matr = compute_distance_matrix(matrix_arrays, num_part=100)
+        matrix_arrays = np.random.random((100, 10, 50))
+        dist_matr = compute_distance_matrix(matrix_arrays, num_part=10)
         self.assertEqual(matrix_arrays.shape[0], dist_matr.shape[0])
 
     def tearDown(self):
